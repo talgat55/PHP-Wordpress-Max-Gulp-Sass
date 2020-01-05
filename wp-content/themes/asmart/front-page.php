97f7d44e7598c49d61b741691ba47aad8eed398c
talgat55 <lightxdesign55@gmail.com>
2020-01-05 21:41:43 +0600

    add block about

--- wp-content/themes/asmart/front-page.php
+++ wp-content/themes/asmart/front-page.php
@@ -4,5 +4,5 @@
 get_header(); ?>
     <h1 class="hide-title"><?=   bloginfo('name'); ?></h1>
 <?php get_template_part('inc/homeSlider'); ?>
-
+<?php get_template_part('inc/homeAbout'); ?>
 <?php get_footer();
---
+++ wp-content/themes/asmart/inc/homeAbout.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lenovo
+ * Date: 05.01.20
+ * Time: 20:49
+ */
+?>
+<section class="home-about">
+    <div class="container">
+        <div class="row">
+            <h2 class="sub-title">
+                Об издании «Максимум»
+            </h2>
+
+        </div>
+        <div class="row  ">
+            <div class="home-about_first col-lg-7 col-md-7 col-sm-12">
+
+                <p class="home-about_first__text">
+                    Информационно - деловой журнал «Максимум» издается с 1998 г. и за эти годы зарекомендовал себя как
+                    издание с высокой деловой репутацией. На наших страницах – статьи и фоторепортажи о деятельности и
+                    развитии промышленных и строительных компаний, предприятий теплоэнергетики и дорожного хозяйства,
+                    финансовых, медицинских и образовательных учреждений.
+                    <br>
+                    <br>
+                    Журнал «Максимум» всегда насыщен разнообразной деловой информацией, полноту его содержания и высокое
+                    качество полиграфии оценили наши многочисленные партнеры и читатели.
+
+                </p>
+                <a href="<?= $url; ?>" class="link link-main">
+                    подробнее
+                </a>
+            </div>
+            <div class="home-about_second col-lg-5 col-md-5 col-sm-12">
+                <ul class="home-about_second_list-facts">
+                    <li class="home-about_second_list-facts__item  d-flex align-items-center justify-content-end">
+                        <div class="home-about_second_list-facts__item__text">
+                            номеров<br>
+                            в год
+                        </div>
+                        <div class="home-about_second_list-facts__item__number">
+                            6
+                        </div>
+                    </li>
+                    <li class="home-about_second_list-facts__item   d-flex align-items-center justify-content-end">
+                        <div class="home-about_second_list-facts__item__text">
+                            лет<br>
+                            на рынке
+                        </div>
+                        <div class="home-about_second_list-facts__item__number">
+                            20
+                        </div>
+                    </li>
+                    <li class="home-about_second_list-facts__item   d-flex align-items-center justify-content-end">
+                        <div class="home-about_second_list-facts__item__text">
+                            тираж<br>
+                            экземпляров
+                        </div>
+                        <div class="home-about_second_list-facts__item__number">
+                            3000
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
